8a418fd456749f76f8bfa1ea8ecdfa1b04c6a928
JugBones <christopher.tjiandra@binus.ac.id>
2024-09-26 15:05:46 +0700

    do exercise 4

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const app = express();
 
 // Specify how to respond to GET /
 app.get('/', (req, res) => {
+    // Get the current date and time
+    const currentDateTime = new Date();
+
     // Run the system `fortune` command and respond with the message
     child_process.exec('fortune', (error, message) => {
-        if(error === null) {
-            res.send(message);
+        if (error === null) {
+            // Build the full response with date and fortune message
+            res.send(`Current Date and Time: ${currentDateTime}<br><br>${message}`);
         } else {
             res.send('Error: ' + error);
         }
-    })
+    });
 });
 
 // Start listening for HTTP requests on port 3000
